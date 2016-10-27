30ad52ed479d406270de874d72dd100fe5fa129f
Nicholas Spyropoulos <Nicholas.Spyropoulos@btfinancialgroup.com>
2016-10-27 12:45:26 +1100

    [fix] ie11 issue with blobs. using blob constructor works

--- createInlineWorker.js
+++ createInlineWorker.js
@@ -3,20 +3,16 @@
 var URL = window.URL || window.webkitURL;
 module.exports = function(content, url) {
 	try {
+		var blob;
 		try {
-			var blob;
-			try { // BlobBuilder = Deprecated, but widely implemented
-				var BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder || window.MSBlobBuilder;
-				blob = new BlobBuilder();
-				blob.append(content);
-				blob = blob.getBlob();
-			} catch(e) { // The proposed API
-				blob = new Blob([content]);
-			}
-			return new Worker(URL.createObjectURL(blob));
-		} catch(e) {
-			return new Worker('data:application/javascript,' + encodeURIComponent(content));
+			blob = new Blob([content], {type: 'application/javascript'});
+		} catch (e) { // Backwards-compatibility
+			var BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
+			blob = new BlobBuilder();
+			blob.append(content);
+			blob = blob.getBlob();
 		}
+		return new Worker(URL.createObjectURL(blob));
 	} catch(e) {
 		return new Worker(url);
 	}
